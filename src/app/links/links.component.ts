f345f1e214baa73c5fd024cfa4bada10d1d34580
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2018-09-19 11:30:54 +0200

    Configuration updated.

--- src/app/links/links.component.ts
+++ src/app/links/links.component.ts
@@ -26,22 +26,22 @@ export class LinksComponent implements OnInit {
           new GroupConfiguration(
             'elVIS_E', 'E-Umgebung', Systemtype.DEV, 'Umfasst das Entwicklungssystem',
             [
-              new LinkConfiguration('elVIS_E_CRM', 'CRM', 'https://elVIS-e.bbrz.at'),
-              new LinkConfiguration('elVIS_E_SP', 'SharePoint', 'https://elVISdms-e.bbrz.at')
+              new LinkConfiguration('elVIS_E_CRM', 'CRM', 'http://elVIS-e.bbrz.at'),
+              new LinkConfiguration('elVIS_E_SP', 'SharePoint', 'http://elVISdms-e.bbrz.at')
             ]
           ),
           new GroupConfiguration(
             'elVIS_Q', 'Q-Umgebung', Systemtype.QS, 'Qualitätssicherungssystem',
             [
-              new LinkConfiguration('elVIS_Q_CRM', 'CRM', 'https://elVIS-q.bbrz.at'),
-              new LinkConfiguration('elVIS_Q_SP', 'SharePoint', 'https://elVISdms-q.bbrz.at')
+              new LinkConfiguration('elVIS_Q_CRM', 'CRM', 'http://elVIS-q.bbrz.at'),
+              new LinkConfiguration('elVIS_Q_SP', 'SharePoint', 'http://elVISdms-q.bbrz.at')
             ]
           ),
           new GroupConfiguration(
             'elVIS_P', 'P-Umgebung', Systemtype.PROD, 'Produktivsystem',
             [
-              new LinkConfiguration('elVIS_P_CRM', 'CRM', 'https://elVIS.bbrz.at'),
-              new LinkConfiguration('elVIS_P_SP', 'SharePoint', 'https://elVISdms.bbrz.at')
+              new LinkConfiguration('elVIS_P_CRM', 'CRM', 'http://elVIS.bbrz.at'),
+              new LinkConfiguration('elVIS_P_SP', 'SharePoint', 'http://elVISdms.bbrz.at')
             ]
           )
         ]
@@ -82,6 +82,51 @@ export class LinksComponent implements OnInit {
             [
               new LinkConfiguration('iRIS_PoCWin2k12_CRM', 'CRM', 'https://irispoc-win2k12.bbrz.at'),
               new LinkConfiguration('iRIS_PoCWin2k12_SP', 'SharePoint', 'https://irispocdms-win2k12.bbrz.at')
+            ]
+          )
+        ]
+      ),
+      new ProjectConfiguration(
+        'FABiS', 'FABiS - TBD',
+        [
+          new GroupConfiguration(
+            'FABiS_E', 'E-Umgebung', Systemtype.DEV, 'Umfasst das Entwicklungssystem',
+            [
+              new LinkConfiguration('FABiS_E_CRM', 'CRM', 'https://fabis-e.office.root.local'),
+              new LinkConfiguration('FABiS_E_SP', 'SharePoint', 'https://fabisdms-e.office.root.local')
+            ]
+          ),
+          new GroupConfiguration(
+            'FABiS_Q', 'Q-Umgebung', Systemtype.QS, 'Qualitätssicherungssystem',
+            [
+              new LinkConfiguration('FABiS_Q_CRM', 'CRM', 'https://fabis-q.office.root.local'),
+              new LinkConfiguration('FABiS_Q_SP', 'SharePoint', 'https://fabisdms-q.office.root.local')
+            ]
+          ),
+          new GroupConfiguration(
+            'FABiS_P', 'P-Umgebung', Systemtype.PROD, 'Produktivsystem',
+            [
+              new LinkConfiguration('FABiS_P_CRM', 'CRM', 'https://fabis.office.root.local'),
+              new LinkConfiguration('FABiS_P_SP', 'SharePoint', 'https://fabisdms.office.root.local')
+            ]
+          )
+        ]
+      ),
+      new ProjectConfiguration(
+        'RFK', 'RFK - Reha-Fachkraft',
+        [
+          new GroupConfiguration(
+            'RFK_E', 'E-Umgebung', Systemtype.DEV, 'Umfasst das Entwicklungssystem',
+            [
+              new LinkConfiguration('RFK_E_CRM', 'CRM', 'http://crm4reha/REHA-COLLEGE-EV01'),
+              new LinkConfiguration('RFK_E_SP', 'SharePoint', 'http://crm4fabdms/reharfk-e')
+            ]
+          ),
+          new GroupConfiguration(
+            'RFK_P', 'P-Umgebung', Systemtype.PROD, 'Produktivsystem',
+            [
+              new LinkConfiguration('RFK_P_CRM', 'CRM', 'http://crm4reha/REHA-COLLEGE-V01'),
+              new LinkConfiguration('RFK_P_SP', 'SharePoint', 'http://crm4fabdms/reharfk')
             ]
           )
         ]
